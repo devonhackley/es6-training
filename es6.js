ebc0d51ea3bca17d5c2e49fd7a561b628fcfb756
devonhackley <devonhackley@gmail.com>
2017-06-09 15:48:17 -0700

    working thru more es6

--- es6.js
+++ es6.js
@@ -19,3 +19,38 @@ var obj = {
   }
 } //best case
 obj.foo();
+
+
+/*Block Scoping */
+
+/** Let vs Var **/
+function foo(x,y){
+  //var tells us that we want to use the variable across the entire function, let is only accessible to the block it was declared
+  if(x > y){
+    let temp =x; //temp is only within the if block
+    x = y
+    y = temp
+  }
+}
+
+/** Closures & Blocks **/
+function foo(x,y){
+  //closure allows a function to remember variables outside of scope
+  for(let i=0; i<10; i++){ // let creates a new i for each iteration
+    $('#btn' +i).click(function(){
+      console.log("button " + i + " was clicked");
+    })
+  }
+}
+
+
+/** Default values **/
+
+function foo(x){ //imperative form
+  x = x || 42; //default values
+}
+function foo(x=42){ //declarative form
+}
+foo.apply(null,[])//will still give us 42
+
+/** Lazy Expressions **/
